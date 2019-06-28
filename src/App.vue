7f0e1ef12d9b7e76eb3bc9e73d75a7d1ee9fe0e3
AcheZeta <ache.mariana@gmail.com>
2019-06-27 19:41:23 -0500

    ADD: Form component with validation

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <v-app>
+    <v-content>
+      <Form/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
+import Form from './components/Form'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Form
+  },
+  data () {
+    return {
+      //
+    }
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
